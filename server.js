783c05c2bd2a7d36a6fa704147bf6b937b34e87a
ustasnov <ustasnov@gmail.com>
2024-04-01 13:22:48 +0700

    Final changes

--- server.js
+++ server.js
@@ -20,7 +20,8 @@ wsServer.on("connection", (ws) => {
 });
 
 //const port = 7070;
-const port = 10000;
+//const port = 10000;
+const port = process.env.PORT || 7070;
 server.listen(port, (err) => {
   if (err) {
     console.log("Error occured:", err);
